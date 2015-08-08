407495aab1dd3e6b148da22c85f714929bf706f4
Toshiaki Asai <toshi.alternative@gmail.com>
2015-08-09 08:33:15 +0900

    Deferred::inspect が有力な情報を返す

--- lib/delayer/deferred/deferred.rb
+++ lib/delayer/deferred/deferred.rb
@@ -23,5 +23,17 @@ module Delayer::Deferred
     def cancel
       deferredable_cancel
       @follow.cancel if @follow.is_a? Deferredable end
+
+    def inspect
+      if ::Delayer::Deferred.debug
+        sprintf("#<%s: %p %s follow:%p stat:%s value:%s>".freeze, self.class, object_id, @backtrace.find{|n|not n.include?("delayer/deferred".freeze)}, @follow ? @follow.object_id : 0, @next_call_stat.inspect, @next_call_value.inspect)
+      else
+        sprintf("#<%s: %p stat:%s value:%s>".freeze, self.class, object_id, @next_call_stat.inspect, @next_call_value.inspect)
+      end
+    end
   end
 end
+
+
+
+

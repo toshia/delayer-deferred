8a344690b239c885fdd584dc4b89b98e63f9a0b0
Hiroshi Hatake <hatake@clear-code.com>
2015-08-13 11:02:16 +0900

    Add eval_all_events to test/helper.rb

--- test/helper.rb
+++ test/helper.rb
@@ -5,3 +5,13 @@ require 'delayer/deferred'
 require 'securerandom'
 require 'set'
 require 'timeout'
+
+def eval_all_events(delayer=Delayer)
+  native = Thread.list
+  result = yield if block_given?
+  while not(delayer.empty? and (Thread.list - native).empty?)
+    delayer.run
+    Thread.pass
+  end
+  result
+end

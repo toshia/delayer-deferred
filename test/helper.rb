07054fb89c836dbc885cc9356cb4cf5b4b38022e
Toshiaki Asai <toshi.alternative@gmail.com>
2021-01-22 08:00:46 +0900

    test tool

--- test/helper.rb
+++ test/helper.rb
@@ -10,5 +10,35 @@ SimpleCov.start do
   add_filter '/test/'
 end
 
+require 'minitest'
+
+module Minitest::Assertions
+  def assert_equal_deferred(expect, defer, message = nil)
+    result = exception = nil
+    eval_all_events do
+      defer.next { |r|
+        result = r
+      }.trap do |exc|
+        exception = exc
+      end
+    end
+    refute exception, 'except success, but failed.'
+    assert_equal expect, result, message
+  end
+
+  def assert_fail_deferred(comparator, defer, message = nil)
+    result = exception = nil
+    eval_all_events do
+      defer.next { |r|
+        result = r
+      }.trap do |exc|
+        exception = exc
+      end
+    end
+    refute expect, 'except fail, but succeed.'
+    assert comparator === exception, message
+  end
+end
+
 require 'delayer/deferred'
 require 'minitest/autorun'
